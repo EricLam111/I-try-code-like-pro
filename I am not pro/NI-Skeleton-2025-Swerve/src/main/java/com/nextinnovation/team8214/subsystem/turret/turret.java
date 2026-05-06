ca553af1a4ab416cb22d6ab8e69fbda35cee7d98
AquariusE <56547433+EricLam111@users.noreply.github.com>
2026-05-07 00:33:13 +0800

    Understand turret Generic IO system, and prewrite
    Today ended

--- I am not pro/NI-Skeleton-2025-Swerve/src/main/java/com/nextinnovation/team8214/subsystem/turret/turret.java
+++ I am not pro/NI-Skeleton-2025-Swerve/src/main/java/com/nextinnovation/team8214/subsystem/turret/turret.java
@@ -2,4 +2,20 @@ public class Turret extends SubsystemBase {
     private final GenericArmIO turretIO;
     private final GenericArmIOInputAutoLogged turretInput = new GenericArmIOInputsAutoLogged();
 
+    private final GenericArmIO pitchIO;
+    private final GenericArmIOInputAutoLogged pitchInput = new GenericArmIOInputsAutoLogged();
+
+    @Getter
+    @AutoLogOutput(key = TurretConfig.LOG_ROOT + "/mode")
+    private ControlMode mode = ControlMode.IDLE;
+
+    @Getter
+    @AutoLogOutput(key = TurretConfig.LOG_ROOT + "/turretOnTarget")
+    private boolean turretOnTarget = false;
+
+    @Getter
+    @AutoLogOutput(key = TurretConfig.LOG_ROOT + "/pitchOnTarget")
+    private boolean pitchOnTarget = false;
+
+    
 }
